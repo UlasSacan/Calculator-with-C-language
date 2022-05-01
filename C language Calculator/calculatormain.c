a1349192a06bf1116f61b7d82ffe973923a1b9a1
Ulaş Saçan <98396775+UlasSacan@users.noreply.github.com>
2022-05-01 14:09:53 +0300

    Add files via upload

---
+++ C language Calculator/calculatormain.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+	int islem;
+	printf("****Hesap makinesi****\n\n");
+	printf("islem seciniz: ");
+	scanf("%s",&islem);
+	
+	int sayi1;
+	int sayi2;
+	int sonuc;
+	
+	
+	switch(islem)
+	{
+		
+		case '+':
+		printf("birinci sayiyi giriniz: ");
+		scanf("%d",&sayi1);
+		
+		printf("ikinci sayiyi giriniz: ");
+		scanf("%d",&sayi2);
+		
+		sonuc = sayi1 + sayi2;
+		printf("%d",sonuc);
+		break;
+		
+		case '-':
+		printf("birinci sayiyi giriniz: ");
+		scanf("%d",&sayi1);
+		
+		printf("ikinci sayiyi giriniz: ");
+		scanf("%d",&sayi2);
+		
+		sonuc = sayi1 - sayi2;
+		printf("%d",sonuc);
+		break;
+		
+		
+		case '*':
+		printf("birinci sayiyi giriniz: ");
+		scanf("%d",&sayi1);
+		
+		printf("ikinci sayiyi giriniz: ");
+		scanf("%d",&sayi2);
+		
+		sonuc = sayi1 * sayi2;
+		printf("%d",sonuc);
+		break;
+		
+		case '/':
+		printf("birinci sayiyi giriniz: ");
+		scanf("%d",&sayi1);
+		
+		printf("ikinci sayiyi giriniz: ");
+		scanf("%d",&sayi2);
+		
+		sonuc = sayi1 / sayi2;
+		printf("%d",sonuc);
+		break;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
